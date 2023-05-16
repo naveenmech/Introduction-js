c4bed2d618db2b2bd09505415543d28ce2fa7b35
naveenmech <vas.naveenkumar99@gmail.com>
2023-05-16 17:43:53 +0530

    add

---
+++ script.js
@@ -0,0 +1,82 @@
+// Introduction to JavaScript
+
+// values and variables
+
+// let country = "India";
+// let continent = "Asia";
+// let population = 10;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// Data types
+
+// let island = true;
+// let language;
+// console.log(typeof island);
+// console.log(typeof language);
+// console.log(typeof country);
+// console.log(typeof continent);
+// console.log(typeof population);
+
+// let const and variables
+
+// let language;
+// language = "Tamil";
+// const country = "India";
+// const continent = "Asia";
+// const island = true;
+// let population = 10;
+// console.log(language)
+// console.log(country);
+// console.log( typeof island);
+// console.log(population);
+
+// let population = 5 + 1;
+// population++;
+// const country = "India";
+// const continent = "Asis";
+// console.log(population / 2);
+// console.log(population);
+// let finlandPopulation = 6;
+// let language = "Tamil";
+// const description =
+//   "Portugal is in Europe, and its 11 million people speak Portuguese";
+// console.log(population < finlandPopulation);
+// console.log(description);
+// const description1 =
+//   country +
+//   ` is in ` +
+//   continent +
+//   ` and its ` +
+//   population +
+//   ` million people speak ` +
+//   language +
+//   `.`;
+// console.log(description1)
+
+// coding challange #1
+
+// let markWeight1 = 78;
+// let markTall1 = 1.69;
+
+// let johnWeight1 = 92;
+// let johnTall1 = 1.95;
+
+// let markWeight2 = 95;
+// let markTall2 = 1.88;
+
+// let johnWeight2 = 85;
+// let johnTall2 = 1.76;
+
+// const markBMI = markWeight1 / markTall1 ** 2;
+// const johnBMI = johnWeight1 / johnTall1 ** 2;
+// const BMI1 = markBMI < johnBMI;
+// const BMI2 = markBMI > johnBMI;
+
+// console.log(markBMI);
+// console.log(johnBMI);
+// console.log(BMI1);
+// console.log(BMI2);
+
+
